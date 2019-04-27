28fea27502c60b135491c953061042dc9de6c518
Agustin Diaz <carlos.agustin.diaz1998@gmail.com>
2019-04-27 19:37:34 -0300

    movimiento nube listo

--- juego.js
+++ juego.js
@@ -1,16 +1,22 @@
 let yN = 0;
 
 //movimiento/ generar nueve
-function nube(y){
-    var maxX = 100;
+function nube(){
+    var maxX = 98;
     var minX = 1;
     x = Math.floor(Math.random() * (maxX - minX  + 1)) + minX;
-    document.getElementById("nube").style.marginTop = (y + "%");
-    document.getElementById("nube").style.marginLeft = (x + "%");
+
+    document.getElementById("nube").style.left = (x + "%");
 };
 function movimientoNube(){
     yN++;
-    nube(yN);
+    if(yN < 85){
+        document.getElementById("nube").style.top = (yN + "%");
+    }
+    else{
+        yN = 0;
+        nube()
+    }
     
 }
-setInterval('movimientoNube()',1000);
+setInterval('movimientoNube()',50);
